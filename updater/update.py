258b03f6f48d09e35d43b4d1003a49217c3db054
XeroHack <73324025+xerohackcom@users.noreply.github.com>
2022-01-07 12:50:29 +0800

    staging update

--- updater/update.py
+++ updater/update.py
@@ -30,11 +30,11 @@ def update_setup():
 			if cdir != "downloads":
 				cdir = f"{get_current_script_path().replace('core/utils.py', f'{cdir}')}"
 				msg_status("WARNING", f"{c_red}Deleting Directory > {c_white}{cdir}")
-				run_cmd(f"rm -rf {cdir}")
+				#run_cmd(f"rm -rf {cdir}")
 	for cfile in chaya_files:
 		cfile = f"{get_current_script_path().replace('core/utils.py', f'{cfile}')}"
 		msg_status("WARNING", f"{c_red}Deleting File > {c_white}{cfile}")
-		run_cmd(f"rm -rf {cfile}")
+		#run_cmd(f"rm -rf {cfile}")
 
 	# move folders and files from /downloads/chaya-runtime/
 	new_chaya_files = get_files_in_dir(f"{get_current_script_path().replace('core/utils.py', f'downloads/Chaya-{runtime}/')}")
@@ -44,13 +44,17 @@ def update_setup():
 	for ndir in new_chaya_dirs:
 		if ndir != "updater":
 			if ndir != "downloads":
-				ndir = f"{get_current_script_path().replace('core/utils.py', f'downloads/Chaya-{runtime}/{ndir}')}"
+				ndirpath = f"downloads/Chaya-{runtime}/{ndir}"
+				ndir = f"{get_current_script_path().replace('core/utils.py', ndirpath)}"
 				msg_status("WARNING", f"{c_red}Moving Directory > {c_white}{ndir}")
-				run_cmd(f"mv {ndir} f'{get_current_script_path().replace('core/utils.py', f'{ndir}')}'")
+				cmd = f"mv {ndir} {get_current_script_path().replace('core/utils.py', '')}"
+				run_cmd(cmd)
 	for nfile in new_chaya_files:
-		nfile = f"{get_current_script_path().replace('core/utils.py', f'downloads/Chaya-{runtime}/{nfile}')}"
+		nfilepath = f"downloads/Chaya-{runtime}/{nfile}"
+		nfile = f"{get_current_script_path().replace('core/utils.py', nfilepath)}"
 		msg_status("WARNING", f"{c_red}Moving File > {c_white}{nfile}")
-		run_cmd(f"mv {nfile} f'{get_current_script_path().replace('core/utils.py', f'{nfile}')}'")
+		cmd = f"mv {nfile} {get_current_script_path().replace('core/utils.py', '')}"
+		run_cmd(cmd)
 
 
 if __name__ == "__main__":
